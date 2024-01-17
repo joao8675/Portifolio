d6dda7ea97f48c4d1bf1dd1b74f2501e60208e61
joao8675 <62406945+joao8675@users.noreply.github.com>
2024-01-17 11:40:06 -0300

    smoothScroll

---
+++ script.js
@@ -0,0 +1,29 @@
+//const menuLinks = document.querySelectorAll('.menu a');
+
+
+//function getDistanceFromTheTop(element) {
+//    const id = element.getAtribute("href");
+//    return document.querySelector(id).offsetTop
+//}
+
+
+
+//function nativeScroll(DistanceFromTheTop) {
+//    window.scroll({
+//        top: DistanceFromTheTop, 
+//        behavior: "smooth",
+//    })
+//}
+
+
+
+//function scrollToSection(event) {
+//    event.preventDefault();
+//    const DistanceFromTheTop = getDistanceFromTheTop(event.target) - 120;
+//    nativeScroll(DistanceFromTheTop);
+//}
+
+
+//menuLinks.forEach((link) => {
+//    link.addEventListener("click", scrollToSection)
+//})
