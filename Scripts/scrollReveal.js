c2db5a2b415f0a43b720d846bf1fe9ce278fdab2
joao8675 <62406945+joao8675@users.noreply.github.com>
2024-01-19 09:50:35 -0300

    scrollReveal

---
+++ Scripts/scrollReveal.js
@@ -0,0 +1,78 @@
+window.ScrollReveal({ reset: true });
+
+ScrollReveal().reveal('.hi1', {
+    rotate:{
+        x:1000
+    }
+})
+ScrollReveal().reveal('.hi2', {
+
+    rotate:{
+        x:1000
+    }
+})
+ScrollReveal().reveal('.hi3', {
+    rotate:{
+        x:1000
+    }
+})
+ScrollReveal().reveal('.hi4', {
+    rotate:{
+        x:1000
+    }
+})
+ScrollReveal().reveal('.hi5', {
+    rotate:{
+        x:1000
+    }
+})
+ScrollReveal().reveal('.hi6', {
+    rotate:{
+        x:1000
+    }
+})
+ScrollReveal().reveal('.hi7', {
+    rotate:{
+        x:1000
+    }
+})
+ScrollReveal().reveal('.hi8', {
+    rotate:{
+        x:1000
+    }
+})
+ScrollReveal().reveal('.hi9', {
+    rotate:{
+        x:1000
+    }
+})
+ScrollReveal().reveal('.hi10', {
+    rotate:{
+        x:1000
+    }
+})
+ScrollReveal().reveal('.hi11', {
+    rotate:{
+        x:1000
+    }
+})
+ScrollReveal().reveal('.hi12', {
+    rotate:{
+        x:1000
+    }
+})
+ScrollReveal().reveal('.hi13', {
+    rotate:{
+        x:1000
+    }
+})
+ScrollReveal().reveal('.hi14', {
+    rotate:{
+        x:1000
+    }
+})
+ScrollReveal().reveal('.hi15', {
+    rotate:{
+        x:1000
+    }
+})
